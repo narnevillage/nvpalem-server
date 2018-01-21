deba7bfbfd222b256f623bb3904e42912b03d780
Chappidi,Srinivas (Contractor) <chappids@oclc.org>
2018-01-21 16:08:47 -0500

    Initial Commit.

---
+++ bhagavanrajupalem/brpalem-web/src/main/groovy/org/srsct/nvpalem/BrpalemWebApplication.groovy
@@ -0,0 +1,12 @@
+package org.srsct.nvpalem
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BrpalemWebApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run BrpalemWebApplication, args
+	}
+}
---
+++ bhagavanrajupalem/brpalem-web/src/test/groovy/org/srsct/nvpalem/BrpalemWebApplicationTests.groovy
@@ -0,0 +1,16 @@
+package org.srsct.nvpalem
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class BrpalemWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

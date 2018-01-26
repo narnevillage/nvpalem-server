2a588efaa5b774d6a77eb15b21eccf7c88f3bdbe
narneramesh05 email = narne.ramesh@gmail.com [core] <narne.village@gmail.com>
2018-01-25 23:02:55 -0600

    Testing Again

--- bhagavanrajupalem/brpalem-rest/src/main/java/org/src/nvpalem/api/impl/RetrieveContentApi.java
+++ bhagavanrajupalem/brpalem-rest/src/main/java/org/src/nvpalem/api/impl/RetrieveContentApi.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.src.nvpalem.api.IRetrieveContentApi;
-import org.src.nvpalem.model.Content;
 import org.src.nvpalem.request.RetrieveContentRequest;
 import org.src.nvpalem.response.RetrieveContentResponse;
 import org.src.nvpalem.service.IRetrieveContentService;

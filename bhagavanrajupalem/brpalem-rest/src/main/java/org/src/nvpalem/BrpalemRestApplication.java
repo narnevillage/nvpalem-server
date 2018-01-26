d18d10aeb4e8aa932ad6193e144e08d7c99f7368
narneramesh05 email = narne.ramesh@gmail.com [core] <narne.village@gmail.com>
2018-01-25 22:48:38 -0600

    Removed Unused Imports. Testing GIT credentials

--- bhagavanrajupalem/brpalem-rest/src/main/java/org/src/nvpalem/BrpalemRestApplication.java
+++ bhagavanrajupalem/brpalem-rest/src/main/java/org/src/nvpalem/BrpalemRestApplication.java
@@ -1,9 +1,7 @@
 package org.src.nvpalem;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication (scanBasePackages = {"org.src.nvpalem"})
 public class BrpalemRestApplication {
